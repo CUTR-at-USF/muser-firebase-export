154c1030984e2242a80eb3159457dce35ed94c6e
Premanshu Yadav <contact@premanshu.com>
2021-08-03 17:51:23 -0400

    feat: add getDateAndTimeFromMillis method to convert Epoch to ISO

--- src/main/java/edu/usf/sas/pal/muser/utils/DateTimeUtil.java
+++ src/main/java/edu/usf/sas/pal/muser/utils/DateTimeUtil.java
@@ -1,5 +1,9 @@
 package edu.usf.sas.pal.muser.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class DateTimeUtil {
 
     /**
@@ -13,6 +17,20 @@ public class DateTimeUtil {
             if (dateStartMillis < dateEndMillis) return true;
             else return false;
         } else return false;
+    }
+
+    /**
+     * Gets the time in the string ISO 8601 UTC format from the provided epoch time
+     * @param millis
+     * @return the time in the string ISO 8601 UTC format from the provided epoch time
+     */
+    public static String getDateAndTimeFromMillis(Long millis) {
+        if (millis == null) return "";
+        Date date = new Date(millis);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        // Set time zone to GMT to obtain timestamp as UTC date
+        sdf.setTimeZone(TimeZone.getTimeZone("Etc/UTC"));
+        return sdf.format(date);
     }
 
 }
